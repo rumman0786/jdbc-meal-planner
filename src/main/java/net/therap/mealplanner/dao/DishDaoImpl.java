c5daca4c5d793cebc650ff417ec709a8259aad43
rumman0786 <rumman.ashraf@gmail.com>
2016-10-20 01:30:46 +0600

    Implemented Dish functions with hibernate replacing JDBC

--- src/main/java/net/therap/mealplanner/dao/DishDaoImpl.java
+++ src/main/java/net/therap/mealplanner/dao/DishDaoImpl.java
@@ -2,6 +2,11 @@ package net.therap.mealplanner.dao;
 
 import net.therap.mealplanner.connection_manager.MysqlConnector;
 import net.therap.mealplanner.entity.Dish;
+import net.therap.mealplanner.entity.MenuType;
+import net.therap.mealplanner.utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -15,43 +20,49 @@ public class DishDaoImpl implements DishDao {
 
     @Override
     public List<Dish> findAll() {
-        List<Dish> dishList = new ArrayList<Dish>();
-        Connection connection = MysqlConnector.getMysqlConnection();
-        Statement stmt = null;
-        try {
-            stmt = connection.createStatement();
-            String sql;
-            sql = "SELECT id, name, calories FROM dish";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String calories = rs.getString("calories");
-                Dish dish = new Dish(name, calories);
-                dish.setId(id);
-                dishList.add(dish);
-            }
-            rs.close();
-            stmt.close();
-            connection.close();
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    stmt.close();
-            } catch (SQLException se2) {
-            }
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
+        SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        List<Dish> dishList = session.createCriteria(Dish.class).list();
+        tx.commit();
+        session.close();
+//        List<Dish> dishList = new ArrayList<Dish>();
+//        Connection connection = MysqlConnector.getMysqlConnection();
+//        Statement stmt = null;
+//        try {
+//            stmt = connection.createStatement();
+//            String sql;
+//            sql = "SELECT id, name, calories FROM dish";
+//            ResultSet rs = stmt.executeQuery(sql);
+//
+//            while (rs.next()) {
+//                int id = rs.getInt("id");
+//                String name = rs.getString("name");
+//                String calories = rs.getString("calories");
+//                Dish dish = new Dish(name, calories);
+//                dish.setId(id);
+//                dishList.add(dish);
+//            }
+//            rs.close();
+//            stmt.close();
+//            connection.close();
+//        } catch (SQLException se) {
+//            se.printStackTrace();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (stmt != null)
+//                    stmt.close();
+//            } catch (SQLException se2) {
+//            }
+//            try {
+//                if (connection != null)
+//                    connection.close();
+//            } catch (SQLException se) {
+//                se.printStackTrace();
+//            }
+//        }
         return dishList;
     }
 
@@ -69,39 +80,46 @@ public class DishDaoImpl implements DishDao {
     public boolean insertDish(Dish dish) {
         List<Dish> dishList = findAll();
         if (!dishList.contains(dish)) {
-            Connection dbConnection = null;
-            PreparedStatement preparedStatement = null;
-
-            String insertTableSQL = "INSERT INTO dish (name, calories) VALUES "
-                    + "(?,?)";
-
-            try {
-                dbConnection = MysqlConnector.getMysqlConnection();
-                preparedStatement = dbConnection.prepareStatement(insertTableSQL);
-                preparedStatement.setString(1, dish.getName());
-                preparedStatement.setString(2, dish.getCalories());
-                // execute insert SQL stetement
-                preparedStatement.executeUpdate();
-
-                return true;
-
-            } catch (SQLException e) {
-
-                System.out.println(e.getMessage());
-
-            } finally {
-                try {
-                    if (preparedStatement != null) {
-                        preparedStatement.close();
-                    }
-
-                    if (dbConnection != null) {
-                        dbConnection.close();
-                    }
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
+            SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+            Session session = sessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.save(dish);
+            tx.commit();
+            session.close();
+            return true;
+//            Connection dbConnection = null;
+//            PreparedStatement preparedStatement = null;
+//
+//            String insertTableSQL = "INSERT INTO dish (name, calories) VALUES "
+//                    + "(?,?)";
+//
+//            try {
+//                dbConnection = MysqlConnector.getMysqlConnection();
+//                preparedStatement = dbConnection.prepareStatement(insertTableSQL);
+//                preparedStatement.setString(1, dish.getName());
+//                preparedStatement.setString(2, dish.getCalories());
+//                // execute insert SQL stetement
+//                preparedStatement.executeUpdate();
+//
+//                return true;
+//
+//            } catch (SQLException e) {
+//
+//                System.out.println(e.getMessage());
+//
+//            } finally {
+//                try {
+//                    if (preparedStatement != null) {
+//                        preparedStatement.close();
+//                    }
+//
+//                    if (dbConnection != null) {
+//                        dbConnection.close();
+//                    }
+//                } catch (SQLException sqlException) {
+//                    sqlException.printStackTrace();
+//                }
+//            }
         }
         return false;
     }
@@ -110,37 +128,44 @@ public class DishDaoImpl implements DishDao {
     public boolean deleteDish(Dish dish) {
         List<Dish> dishList = findAll();
         if (dishList.contains(dish)) {
-            Statement stmt = null;
-            Connection dbConnection = MysqlConnector.getMysqlConnection();
-            PreparedStatement preparedStatement = null;
-
-            String deleteSQL = "DELETE FROM dish WHERE id = ?";
-
-            try {
-                preparedStatement = dbConnection.prepareStatement(deleteSQL);
-                preparedStatement.setInt(1, dish.getId());
-                // execute delete SQL stetement
-                preparedStatement.executeUpdate();
-
-                System.out.println("Record is deleted!");
-                return true;
-            } catch (SQLException e) {
-
-                System.out.println(e.getMessage());
-
-            } finally {
-                try {
-                    if (preparedStatement != null) {
-                        preparedStatement.close();
-                    }
-
-                    if (dbConnection != null) {
-                        dbConnection.close();
-                    }
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
+            SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+            Session session = sessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.delete(dish);
+            tx.commit();
+            session.close();
+            return true;
+//            Statement stmt = null;
+//            Connection dbConnection = MysqlConnector.getMysqlConnection();
+//            PreparedStatement preparedStatement = null;
+//
+//            String deleteSQL = "DELETE FROM dish WHERE id = ?";
+//
+//            try {
+//                preparedStatement = dbConnection.prepareStatement(deleteSQL);
+//                preparedStatement.setInt(1, dish.getId());
+//                // execute delete SQL stetement
+//                preparedStatement.executeUpdate();
+//
+//                System.out.println("Record is deleted!");
+//                return true;
+//            } catch (SQLException e) {
+//
+//                System.out.println(e.getMessage());
+//
+//            } finally {
+//                try {
+//                    if (preparedStatement != null) {
+//                        preparedStatement.close();
+//                    }
+//
+//                    if (dbConnection != null) {
+//                        dbConnection.close();
+//                    }
+//                } catch (SQLException sqlException) {
+//                    sqlException.printStackTrace();
+//                }
+//            }
         }
         return false;
     }
@@ -150,39 +175,46 @@ public class DishDaoImpl implements DishDao {
 
         List<Dish> dishList = findAll();
         if (dishList.contains(dish)) {
-            Connection dbConnection = null;
-            PreparedStatement preparedStatement = null;
-            String insertTableSQL = "UPDATE dish SET name = ?,calories = ? WHERE id = ? ;";
-
-            try {
-                dbConnection = MysqlConnector.getMysqlConnection();
-                preparedStatement = dbConnection.prepareStatement(insertTableSQL);
-
-                preparedStatement.setString(1, dish.getName());
-                preparedStatement.setString(2, dish.getCalories());
-                preparedStatement.setString(3, String.valueOf(dish.getId()));
-                // execute insert SQL stetement
-                preparedStatement.executeUpdate();
-
-                return true;
-
-            } catch (SQLException e) {
-
-                System.out.println(e.getMessage());
-
-            } finally {
-                try {
-                    if (preparedStatement != null) {
-                        preparedStatement.close();
-                    }
-
-                    if (dbConnection != null) {
-                        dbConnection.close();
-                    }
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
+            SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.update(dish);
+            transaction.commit();
+            session.close();
+            return true;
+//            Connection dbConnection = null;
+//            PreparedStatement preparedStatement = null;
+//            String insertTableSQL = "UPDATE dish SET name = ?,calories = ? WHERE id = ? ;";
+//
+//            try {
+//                dbConnection = MysqlConnector.getMysqlConnection();
+//                preparedStatement = dbConnection.prepareStatement(insertTableSQL);
+//
+//                preparedStatement.setString(1, dish.getName());
+//                preparedStatement.setString(2, dish.getCalories());
+//                preparedStatement.setString(3, String.valueOf(dish.getId()));
+//                // execute insert SQL stetement
+//                preparedStatement.executeUpdate();
+//
+//                return true;
+//
+//            } catch (SQLException e) {
+//
+//                System.out.println(e.getMessage());
+//
+//            } finally {
+//                try {
+//                    if (preparedStatement != null) {
+//                        preparedStatement.close();
+//                    }
+//
+//                    if (dbConnection != null) {
+//                        dbConnection.close();
+//                    }
+//                } catch (SQLException sqlException) {
+//                    sqlException.printStackTrace();
+//                }
+//            }
         }
         return false;
     }
